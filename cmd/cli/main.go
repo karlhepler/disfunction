ccb654a633df8ed2816adda37eb6d60ee6732767
Karl Hepler <karl.hepler@gmail.com>
2024-12-20 22:42:58 -0500

    cli is looking good

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,21 +5,62 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v3"
 )
 
 func main() {
 	cmd := &cli.Command{
-		Name:  "random",
-		Usage: "Display a random function and its author from all repositories.",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("Hello friend!")
-			return nil
+		Name:  "disfunction",
+		Usage: "What dis function?",
+		Commands: []*cli.Command{
+			{
+				Name: "random",
+				Usage: `Display a random function
+chosen from all repositories within a date range.`,
+				Flags: []cli.Flag{
+					&cli.TimestampFlag{
+						Name: "since",
+						Usage: `Only commits after this date will be returned.
+	This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
+	`,
+						Value:       GetStartOfDay(time.Now()),
+						DefaultText: "beginning of today",
+					},
+					&cli.TimestampFlag{
+						Name: "until",
+						Usage: `Only commits before this date will be returned.
+	This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
+	`,
+						Value:       time.Now(),
+						DefaultText: "now",
+					},
+					&cli.StringSliceFlag{
+						Name: "kind",
+						Usage: `Filter kinds of functions by this list.
+	supported kinds: new`,
+					},
+				},
+				Action: func(context.Context, *cli.Command) error {
+					fmt.Println("Hello friend!")
+					return nil
+				},
+			},
 		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func GetStartOfDay(t time.Time) time.Time {
+	return time.Date(
+		t.Year(),
+		t.Month(),
+		t.Day(),
+		0, 0, 0, 0, // hour, minute, second, nanosecond
+		t.Location(),
+	)
 }
