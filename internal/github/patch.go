ce58719dde7eedfa93845979d5ca4763c5afb0c8
Karl Hepler <karl.hepler@gmail.com>
2024-12-24 16:08:40 -0500

    it works

--- internal/github/patch.go
+++ internal/github/patch.go
@@ -3,8 +3,6 @@ package github
 import (
 	"context"
 	"fmt"
-
-	"github.com/google/go-github/v67/github"
 )
 
 func (c *Client) ListPatchesByCommits(ctx context.Context, commits <-chan Commit) (<-chan Patch, <-chan error) {
@@ -13,24 +11,23 @@ func (c *Client) ListPatchesByCommits(ctx context.Context, commits <-chan Commit
 		defer close(outchan)
 		defer close(errchan)
 
-		opt := &github.ListOptions{PerPage: 100}
-
 		for commit := range commits {
-			c.Log.Debugf("GitHub.Repositories.GetCommit(owner=%s, repo=%s, sha=%s, page=%d)", commit.Repo.Owner, commit.Repo.Name, *commit.SHA, opt.Page)
-			meta, res, err := c.GitHub.Repositories.GetCommit(ctx, commit.Repo.Owner.String(), commit.Repo.Name, *commit.SHA, opt)
+			c.Log.Debugf("GitHub.Repositories.GetCommit(owner=%s, repo=%s, sha=%s)", commit.Repo.Owner, commit.Repo.Name, *commit.SHA)
+			meta, res, err := c.GitHub.Repositories.GetCommit(ctx, commit.Repo.Owner.String(), commit.Repo.Name, *commit.SHA, nil)
 			if err != nil {
 				errchan <- fmt.Errorf("error getting commit; repo=%s sha=%s", commit.Repo, *commit.SHA)
 			}
 
 			for _, file := range meta.Files {
-				c.Log.Debugf("\tfile=%s", *file.Filename)
-				outchan <- Patch{commit, *file.Patch}
+				if file.Patch != nil {
+					c.Log.Debugf("\tfile=%s", *file.Filename)
+					outchan <- Patch{commit, *file.Patch}
+				}
 			}
 
-			if res == nil || res.NextPage == 0 {
+			if res == nil {
 				break
 			}
-			opt.Page = res.NextPage
 		}
 	}()
 	return outchan, errchan
